e9fbf7bd8c2dc4d07b8e9f86310c3a7035cd2ee3
shvpnd <171777958+shvpnd@users.noreply.github.com>
2024-10-18 12:18:26 +0530

    Fix #3: Keyboard shortcuts
    1. Added the following shortcuts:
        - `s`: Open search
        - `←`: Play next episode
        - `→`: Play previous episode
        - `w`: Watch now (on details page)
        - `d`: Go to Daikon homepage
    2. Added shortcut popup in `index.html`. Press `h` to open.
    
    Co-authored-by: akshvy <aklshayroy217@gmail.com>

--- script/search.js
+++ script/search.js
@@ -1,11 +1,15 @@
+let currentFocus = -1;
+
 document.getElementById('searchBar').addEventListener('input', async function () {
     const query = this.value;
     if (query.length < 1) {
         document.getElementById('suggestions').style.display = 'none';
         return;
     }
+
     const response = await fetch(`https://amvstrm-api-olive.vercel.app/api/v2/search?q=${query}`);
     const data = await response.json();
+
     const suggestions = data.results.map(item => `
         <a href="./details.html?id=${item.id}" class="suggestion-item">
             <img src="${item.coverImage.extraLarge}" alt="${item.title.userPreferred}">
@@ -16,8 +20,55 @@ document.getElementById('searchBar').addEventListener('input', async function ()
             </div>
         </a>
     `).join('');
+
     document.getElementById('suggestions').innerHTML = suggestions;
     document.getElementById('suggestions').style.display = 'block';
+    currentFocus = -1;
+});
+
+function addActive(suggestions) {
+    removeActive(suggestions);
+    if (currentFocus >= suggestions.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = suggestions.length - 1;
+    suggestions[currentFocus].classList.add('active');
+}
+
+function removeActive(suggestions) {
+    for (const suggestion of suggestions) {
+        suggestion.classList.remove('active');
+    }
+}
+
+function clickActiveSuggestion(suggestions) {
+    if (currentFocus > -1 && suggestions[currentFocus]) {
+        suggestions[currentFocus].click();
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    const searchBar = document.getElementById('searchBar');
+    const suggestionsContainer = document.getElementById('suggestions');
+    const suggestions = suggestionsContainer.getElementsByClassName('suggestion-item');
+
+    if (event.key === 's' && document.activeElement !== searchBar) {
+        event.preventDefault();
+        searchBar.focus();
+    }
+
+    if (document.activeElement === searchBar) {
+        if (event.key === 'ArrowDown') {
+            currentFocus++;
+            addActive(suggestions);
+            event.preventDefault();
+        } else if (event.key === 'ArrowUp') {
+            currentFocus--;
+            addActive(suggestions);
+            event.preventDefault();
+        } else if (event.key === 'Enter') {
+            event.preventDefault();
+            clickActiveSuggestion(suggestions);
+        }
+    }
 });
 
 document.addEventListener('click', function (event) {
